87bdfddbc3ba2c29c5e9b063fdec83b83da0c168
Matias Risco <matiasriscolagos@gmail.com>
2023-10-26 23:39:35 +0200

    Add header in component App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,9 @@ import AppStyled from "./AppStyled";
 const App = (): React.ReactElement => {
   return (
     <AppStyled className="app">
+      <header className="header">
+        <h1>Perezoso Formulario</h1>
+      </header>
       <Form />
     </AppStyled>
   );
