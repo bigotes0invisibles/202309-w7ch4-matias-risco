d64449a1ba995c3685808039d7866e115aa5af6f
Matias Risco <matiasriscolagos@gmail.com>
2023-10-27 08:54:56 +0200

    Add GlobalStyle and implemented in main

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import PerezosoProviderWrapper from "./features/perezosos/store/PerezosoProviderWrapper";
+import MainTheme from "./mainTheme";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./GlobalStyle";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <PerezosoProviderWrapper>
-      <App />
+      <ThemeProvider theme={MainTheme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </PerezosoProviderWrapper>
   </React.StrictMode>,
 );
